c78675b9b75d3012364c3690f17f8d412bc982d5
Ken-2100 <166726504+Ken-2100@users.noreply.github.com>
2024-06-10 21:42:22 +0800

    first commit react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import NavHead from './activity/NavHead';
+
+// import Name from './components/Name';
+
+// import {New1, New2} from './New';
 
 function App() {
+  const element = (
+    <h1>WORLD HELLO!</h1>
+  )
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <NavHead/>
     </div>
   );
 }
