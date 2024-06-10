ae3aa4f80c1eff8c2051af241fe122e3e9f62bf6
Ken-2100 <166726504+Ken-2100@users.noreply.github.com>
2024-06-10 22:59:32 +0800

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import Nav1 from './activity/Nav';
+import Carousel from './activity/Carousel';
+import Card from './activity/Card';
+import Footer from './activity/Footer';
 
 function App() {
   
   return (
     <div className="App">
      <Nav1/>
+     <Carousel/>
+    <center>
+     <Card/>
+     <Card/>
+     <Card/>
+     </center>
+     <Footer/>
+     
+   
 
     </div>
   );
